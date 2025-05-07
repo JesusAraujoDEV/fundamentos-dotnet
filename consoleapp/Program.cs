8f8ff4373f663cf990894192c751cf9e33a3ee0b
jesusaraujodev <j2a0a0a5@gmail.com>
2025-05-07 18:48:51 -0400

    Add Humanizer package and update console prompts for user input y el CIERRE

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -1,8 +1,10 @@
-﻿Console.WriteLine("Por favor ingrese un nombre epico!!: ");
-var nombre = Console.ReadLine();
-Console.WriteLine("Por favor ingrese su cargo en la empresa!!: ");
-var cargo = Console.ReadLine();
-Console.WriteLine("Por favor ingrese tu edad!!: ");
-var edad = Console.ReadLine();
+﻿using Humanizer;
 
-Console.WriteLine($"Mi nombre es: {nombre} y mi cargo es: {cargo} y tengo: {edad} años");
+Console.Write("Por favor ingrese un nombre epico!!: ");
+var nombre = Console.ReadLine();
+Console.Write("Por favor ingrese su cargo en la empresa!!: ");
+var cargo = Console.ReadLine();
+Console.Write("Por favor ingrese tu edad!!: ");
+var edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Mi nombre es: {nombre} y mi cargo es: {cargo} y tengo: {edad.ToWords(new System.Globalization.CultureInfo("es"))} años");
