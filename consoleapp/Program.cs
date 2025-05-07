d61d70c1320d857be95d78e3f29df65f6468abd8
jesusaraujodev <j2a0a0a5@gmail.com>
2025-05-07 18:21:03 -0400

    Remove .NET project files and initial program code.

---
+++ consoleapp/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Por favor ingrese un nombre epico!!: ");
+var nombre = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su cargo en la empresa!!: ");
+var cargo = Console.ReadLine();
+Console.WriteLine("Por favor ingrese tu edad!!: ");
+var edad = Console.ReadLine();
+
+Console.WriteLine($"Mi nombre es: {nombre} y mi cargo es: {cargo} y tengo: {edad} años");
